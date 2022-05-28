e1ac3cf9395c46574d840ea82c69340cb3cc178c
Rubén Juárez <91731294+rubences@users.noreply.github.com>
2022-05-28 17:24:58 +0000

    Cambios en Pesos

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pnd
+import numpy as np
+import JMPEstadisticas as jmp
+import os
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+filename = os.path.join(current_dir, 'datos.csv')
+raw_data = open(filename)
+data = np.loadtxt(raw_data, delimiter=";",skiprows=1)
+data=pnd.DataFrame({'Pesos':data})
+stats = jmp.JMPEstadisticas(data["Pesos"])
+stats.analisisCaracteristica()
